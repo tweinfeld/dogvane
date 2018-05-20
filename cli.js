d78c2b3a7afb84e93e1e97159b76a1e591ca6aab
motherfunctor <28810377+motherfunctor@users.noreply.github.com>
2018-05-20 20:57:39 +0300

    Updated cli

--- cli.js
+++ cli.js
@@ -3,4 +3,4 @@ const
     path = require('path'),
     child_process = require('child_process');
 
-child_process.fork(path.join(__dirname, "server.js"));
+child_process.fork(path.join(__dirname, "server.js"), process.argv.slice(2), { cwd: __dirname });
